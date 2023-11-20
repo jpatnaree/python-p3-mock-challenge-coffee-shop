7733a1f773a13d460ea21756d190a96311c59cb2
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-11-20 10:31:41 -0500

    mock challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,80 @@
+from statistics import mean
+
 class Coffee:
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if not hasattr(self, '_name'):
+            if isinstance(new_name, str) and len(new_name) >= 3:
+                self._name = new_name
+            else:
+                raise Exception('must be a string & at least 3 characters long')
+        else:
+            raise Exception('cannot be change')
         
     def orders(self):
-        pass
+        return [o for o in Order.all if o.coffee == self]
     
     def customers(self):
-        pass
+        return list(set([o.customer for o in Order.all if o.coffee == self]))
     
     def num_orders(self):
-        pass
+        return len(self.orders())
     
     def average_price(self):
-        pass
+        all_price = [o.price for o in Order.all if o.coffee == self]
+        return mean(all_price)
 
 class Customer:
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and 1 <= len(new_name) <= 15:
+            self._name = new_name
+        else:
+            raise Exception('must be a string & have 1 - 15 characters')
         
     def orders(self):
-        pass
+        return [o for o in Order.all if o.customer == self]
     
     def coffees(self):
-        pass
+        return list(set([o.coffee for o in Order.all if o.customer == self]))
     
     def create_order(self, coffee, price):
-        pass
-    
+        return Order(self, coffee, price)
+
 class Order:
+
+    all = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self, new_price):
+        if not hasattr(self,'_price'):
+            if isinstance(new_price, float) and 1.0 <= new_price <= 10.0:
+                self._price = new_price
+            else:
+                raise Exception('Wrong!')
+        else:
+            raise Exception('cannot be changed')
